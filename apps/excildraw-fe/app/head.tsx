0175aaaad935909091b7ffca12368af5cb6aba7d
Harinandan <harinandannn@gamil.com>
2025-08-23 21:40:51 +0530

    title folder

---
+++ apps/excildraw-fe/app/head.tsx
@@ -0,0 +1,11 @@
+
+export default function Head() {
+  return (
+    <>
+      <title>Collab canvas</title>
+      <meta name="description" content="Collaborative online whiteboard" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
